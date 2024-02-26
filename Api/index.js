aa01f7c2b96b9e8c28b2cf152d70e4c05a884205
Webgoonie <webgoonie@gmail.com>
2024-02-26 01:22:45 -0500

    Created Register Post And Prisma Create With Schema on Express Api

--- Api/index.js
+++ Api/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+const { PrismaClient } = require("@prisma/client")
+
+const prisma = new PrismaClient()
+
 app.use(express.json())
 
 const port = 4000
@@ -10,12 +14,32 @@ app.get('/test', (req, res) => {
     res.json('test okay')
 })
 
-app.post('/register', (req, res) => {
-    res.json('register okay')
+app.get('/usersAll', async (req, res) => {
+    
+    const allUsers = await prisma.user.findMany();
+    res.json(allUsers);
+})
+
+app.post('/register', async (req, res) => {
+
+    console.log('register', req.body)
+
+    const newUser = await prisma.user.create({
+        data: {
+            email: req.body.email,
+            hashedPassword: req.body.password,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+        }
+    })
+
+    res.json(newUser);
 })
 
 
 app.listen(port, () => {
     console.log('Api Listening on Port', port)
 })
+
+
 
